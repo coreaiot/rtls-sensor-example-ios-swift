54cd61577d1bcb643442c2320c480c8cbe168226
Yu Chen <yu.chen@live.ie>
2023-11-08 14:18:56 +0800

    v2.2.0

--- rtls-sensor-example-ios-swift/ViewController.swift
+++ rtls-sensor-example-ios-swift/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
     let ble = Ble()
     var started = false
 
+    @IBOutlet weak var mac: UITextField!
+    @IBOutlet weak var alarm: UISwitch!
+    @IBOutlet weak var moving: UISwitch!
+    
     @IBOutlet weak var btn: UIButton!
     @IBAction func btnClick(_ sender: Any) {
         if !started {
@@ -27,13 +31,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        UIDevice.current.isBatteryMonitoringEnabled = true
     }
 
     func generateAdvertisingData() -> [String: Any] {
-        let id: UInt16 = 0x1234
-        let alarm: UInt8 = 0
-        let battery: UInt8 = 5
-        let data = coreaiot_generate_service_uuids(id, alarm, battery);
+        let id: UInt16 = UInt16(mac.text ?? "1234", radix: 16) ?? 0x1234
+        let alarm: UInt8 = self.alarm.isOn ? 1 : 0
+        let battery: UInt8 = UInt8(UIDevice.current.batteryLevel * 10)
+        let isStatic: UInt8 = moving.isOn ? 0 : 1
+        let data = coreaiot_generate_service_uuids(id, alarm, battery, isStatic);
         let len = Int(COREAIOT_NUMBER_OF_SERVICE_UUIDS);
         var arr = [UInt16](repeating: 0, count: len)
 
@@ -48,7 +54,8 @@ class ViewController: UIViewController {
         dump(uuids)
 
         return [
-            CBAdvertisementDataServiceUUIDsKey: uuids
+            CBAdvertisementDataServiceUUIDsKey: uuids,
+            CBAdvertisementDataTxPowerLevelKey: CBAdvertisementDataTxPowerLevelKey
         ]
     }
 }
--- rtls-sensor-example-ios-swift/rtls-sensor-example-ios-swift-Bridging-Header.h
+++ rtls-sensor-example-ios-swift/rtls-sensor-example-ios-swift-Bridging-Header.h
@@ -7,4 +7,4 @@
 #define COREAIOT_NUMBER_OF_SERVICE_UUIDS 13
 
 uint16_t *coreaiot_generate_service_uuids(uint16_t id, uint8_t alarm,
-                                          uint8_t battery);
+                                          uint8_t battery, uint8_t is_static);
