05f6201573b955ba808d7c43ee7dec2d3324d44d
Yu Chen <yu.chen@live.ie>
2023-11-09 17:15:21 +0800

    feat channels

--- rtls-sensor-example-ios-swift/ViewController.swift
+++ rtls-sensor-example-ios-swift/ViewController.swift
@@ -11,18 +11,38 @@ import CoreBluetooth
 class ViewController: UIViewController {
     let ble = Ble()
     var started = false
+    var channel: UInt8 = 0
 
     @IBOutlet weak var mac: UITextField!
     @IBOutlet weak var alarm: UISwitch!
     @IBOutlet weak var moving: UISwitch!
-    
+    @IBOutlet weak var channelButton: UIButton!
     @IBOutlet weak var btn: UIButton!
+    
+    @IBOutlet weak var channel37: UICommand!
+    @IBOutlet weak var channel38: UICommand!
+    @IBOutlet weak var channel39: UICommand!
+    @IBOutlet weak var content: UITextView!
+    
+    @IBAction func channelSelect(_ sender: UICommand) {
+        channelButton.setTitle(sender.title, for: .normal)
+        
+        if sender.title.starts(with: "38") {
+            channel = 1
+        } else if sender.title.starts(with: "39") {
+            channel = 2
+        } else {
+            channel = 0
+        }
+    }
+    
     @IBAction func btnClick(_ sender: Any) {
         if !started {
             ble.advertisingData = generateAdvertisingData()
             try! ble.start()
         } else {
             ble.stop()
+            content.text = ""
         }
         started = !started
         btn.setTitle(started ? "Stop" : "Start", for: .normal)
@@ -39,7 +59,14 @@ class ViewController: UIViewController {
         let alarm: UInt8 = self.alarm.isOn ? 1 : 0
         let battery: UInt8 = UInt8(UIDevice.current.batteryLevel * 10)
         let isStatic: UInt8 = moving.isOn ? 0 : 1
-        let data = coreaiot_generate_service_uuids(id, alarm, battery, isStatic);
+        
+        print("mac:", mac.text ?? "1234")
+        print("id:", id)
+        print("alarm:", alarm)
+        print("battery:", battery)
+        print("channel:", channel)
+        
+        let data = coreaiot_generate_service_uuids(id, alarm, battery, channel, isStatic);
         let len = Int(COREAIOT_NUMBER_OF_SERVICE_UUIDS);
         var arr = [UInt16](repeating: 0, count: len)
 
@@ -51,11 +78,10 @@ class ViewController: UIViewController {
             return CBUUID(string: String(format: "%04x", x))
         }
         
-        dump(uuids)
+        content.text = uuids.description;
 
         return [
-            CBAdvertisementDataServiceUUIDsKey: uuids,
-            CBAdvertisementDataTxPowerLevelKey: CBAdvertisementDataTxPowerLevelKey
+            CBAdvertisementDataServiceUUIDsKey: uuids
         ]
     }
 }
